fe5883fce827255499757bc1b59d92496c656c52
ChillyWilly <noreply>
2021-01-19 01:58:02 -0500

    date editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,24 +44,28 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
-
+// task was clicked
 $(".list-group").on("click", "p", function() {
+  // get current text in task
   var text = $(this)
   .text()
   .trim();
+  // create textarea for user to modify task
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
+  // swap <p> with <textarea>
   $(this).replaceWith(textInput);
+  // automatically focus on <textarea>
   textInput.trigger("focus");
-  console.log(text);
 });
 
+// value of task was changed
 $(".list-group").on("blur", "textarea", function() {
   var text = $(this)
     .val()
     .trim();
-  // get the parent ul's id attribute
+  // get the parent ul's id attribute. all ul's have id's list-xxx where xxx is key in tasks obj
   var status = $(this)
     .closest(".list-group")
     .attr("id")
@@ -70,6 +74,7 @@ $(".list-group").on("blur", "textarea", function() {
   var index = $(this)
     .closest(".list-group-item")
     .index();
+  // update task in array and save to localstorage. what is .text??? when tasks are created the ul elemenet is made an object with task: and date: keys
   tasks[status][index].text = text;
   saveTasks();
   // recreate p element instead of textarea
@@ -79,6 +84,48 @@ $(".list-group").on("blur", "textarea", function() {
   $(this).replaceWith(taskP);
 })
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  // swap out elements
+  $(this).replaceWith(dateInput);
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+//value of date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  //get current text
+  var date = $(this)
+    .val()
+    .trim();
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
