5ca2ed8fb37eef9033e69bdcbc4365455a56b7eb
ChillyWilly <noreply>
2021-01-22 13:15:39 -0500

    dates are checked upon creation or changed and background is set according to imminency

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 var tasks = {};
 
+//taskEl is taskLi
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  console.log(date);
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  // if current moment is after hour 17 on the due date for the taskEl apply new class
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger")
+    // absolute value of number so we arent checking against a negative difference since the due date is some point in the future
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning"); 
+  }
+}
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -13,6 +31,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -86,23 +106,30 @@ $(".list-group").on("blur", "textarea", function() {
 
 // due date was clicked
 $(".list-group").on("click", "span", function() {
-  // get current text
+  // get current text. 'this' refers to span
   var date = $(this)
     .text()
     .trim();
+
   // create new input element
   var dateInput = $("<input>")
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
+   
+
   // swap out elements
   $(this).replaceWith(dateInput);
-  // automatically focus on new element
+  // jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1
+  });
+  // automatically brings up the calendar
   dateInput.trigger("focus");
 });
 
 //value of date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   //get current text
   var date = $(this)
     .val()
@@ -125,12 +152,23 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .text(date);
   // replace input with span element
   $(this).replaceWith(taskSpan);
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
+  // utilize datepicker() to bring up a calendar interface to select date
+  $("#modalDueDate").datepicker({
+    // date must at least be the next day
+    minDate: 1,
+    // upon close trigger the change event (in case they don't want to change the date so the element switches back to span)
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
 });
 
 // modal is fully visible
